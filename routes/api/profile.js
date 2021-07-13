9050215e15b04e076f9ee7fcf597c2bec64bc82a
rawendra <rawendra@gmail.com>
2021-07-13 08:38:10 +0530

    adding the code for profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,23 @@ const router = express.Router();
 const passport = require("passport");
 const Profile = require("../../models/Profile");
 const profileValidator = require("../../validator/profileValidator");
+
+router.get(
+  "/getprofile/:githubusername",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { githubusername } = req.params;
+    Profile.findOne({ githubusername: githubusername })
+      .then((profile) => {
+        if (!profile) {
+          return res.status.status(400).json({ msg: "no profile found" });
+        }
+        res.status(200).json({ response: { profile } });
+      })
+      .catch((err) => res.status(400).json({ err }));
+  }
+);
+
 router.post(
   "/createUpdateProfile",
   passport.authenticate("jwt", { session: false }),
@@ -29,7 +46,10 @@ router.post(
       githubusername,
       social,
     };
-    const validationresponse = profileValidator({skills:profileFields.skills,location});
+    const validationresponse = profileValidator({
+      skills: profileFields.skills,
+      location,
+    });
     if (validationresponse.isError) {
       return res.status(500).json(validationresponse);
     }
