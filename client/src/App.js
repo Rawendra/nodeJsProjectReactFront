ce8a15e69cb242e802c896437a5590b4b8bf57b8
rawendra <rawendra@gmail.com>
2021-07-20 19:21:00 +0530

    adding the barebone react js code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MY react APP</h1>
+    
+    </div>
+  );
+} 
+
+export default App;
