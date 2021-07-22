e4a1e8d3081b85a52adcc56715099c2bf498e33d
rawendra <rawendra@gmail.com>
2021-07-22 13:10:41 +0530

    adding the ui code for register and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
-import Navbar from './components/layout/navbar';
-import Footer from './components/layout/footer';
-import Landing from './components/layout/landing';
+import "./App.css";
+import Navbar from "./components/layout/navbar";
+import Footer from "./components/layout/footer";
+import Landing from "./components/layout/landing";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    
-    </div>
+    <Router>
+      {" "}
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
-} 
+}
 
 export default App;
---
+++ client/src/components/auth/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <span>this is login</span>
+      </div>
+    );
+  }
+}
